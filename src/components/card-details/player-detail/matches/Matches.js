ed53d4fe5fa001bfa2a095b0b77c24c76ce0a728
bartukocakara <kocakarabartu@gmail.com>
2021-06-27 22:33:58 +0300

    Teams Card Style

--- src/components/card-details/player-detail/matches/Matches.js
+++ src/components/card-details/player-detail/matches/Matches.js
@@ -2,27 +2,19 @@ import React from 'react'
 import {
     Link
   } from "react-router-dom";
-
+import Avatar from '@material-ui/core/Avatar';
+import AvatarGroup from '@material-ui/lab/AvatarGroup';
 const Matches = () => {
     return (
         <div class="card component-card_8">
-            <div class="card-body">
+            <div class="card-body p-3">
                 <div class="progress-order">
-                    <div class="progress-order-header">
-                        <div class="row">
-                            <div class="col-md-6 col-sm-6 col-12">
-                                <h6 className="text-dark"><Link to="ended-match-detail">Details</Link></h6>
-                            </div>
-                            <div class="col-md-6 pl-0 col-sm-6 col-12 text-right">
-                                <span class="badge badge-info">IN PROGRESS</span>
-                            </div>
-                        </div>
-                    </div>
                     <div class="progress-order-body">
                         <div class="row mt-4">
-                            <div class="col-md-12 d-flex">
-                                <ul class="list-inline badge-collapsed-img mb-0 mb-3">
-                                
+                            <div class="col-md-12 row">
+                                <ul class="list-inline col-md-4 badge-collapsed-img mb-0 mb-3">
+                                <h6 className="text-dark"><Link to="ended-match-detail">Details</Link></h6>
+
                                     <li class="list-inline-item">
                                         <Link to="team-detail">Team 1 </Link>
                                     </li>
@@ -30,9 +22,18 @@ const Matches = () => {
                                         <Link to="team-detail">Team 2</Link>
                                     </li>
                                 </ul>
-                                <div className="col-5 court-name">
+                                <div className="col-5 col-md-4 court-name">
+                                <span class="badge badge-info">IN PROGRESS</span>
+
                                     <h6 className="ml-3">Alsancak Spor salonu</h6>
                                 </div>
+                                <AvatarGroup max={4} className="col-md-4">
+                                    <Avatar alt="Remy Sharp" src="assets/images/faces/2.jpg" />
+                                    <Avatar alt="Travis Howard" src="assets/images/faces/2.jpg" />
+                                    <Avatar alt="Cindy Baker" src="assets/images/faces/2.jpg" />
+                                    <Avatar alt="Agnes Walker" src="assets/images/faces/2.jpg" />
+                                    <Avatar alt="Trevor Henderson" src="assets/images/faces/2.jpg" />
+                                </AvatarGroup>
                             </div>
                             <div class="col-md-12 text-right">
                                 <span class=" p-o-percentage mr-4">60%</span>
--- src/components/card-details/player-detail/teams/Teams.js
+++ src/components/card-details/player-detail/teams/Teams.js
@@ -6,25 +6,24 @@ import {
   } from "react-router-dom";
 const Teams = () => {
     return (
-        <div className="col-3 feed-card player-details-teams m-auto border rounded">
-        <div className="card">
-            <h4 className="card-title text-center mt-2">Bornova Team</h4>
-            <div className="card-content card-body">
-                <AvatarGroup max={3} >
-                    <Avatar alt="Remy Sharp" src="assets/images/faces/2.jpg" />
-                    <Avatar alt="Travis Howard" src="assets/images/faces/2.jpg" />
-                    <Avatar alt="Cindy Baker" src="assets/images/faces/2.jpg" />
-                    <Avatar alt="Agnes Walker" src="assets/images/faces/2.jpg" />
-                    <Avatar alt="Trevor Henderson" src="assets/images/faces/2.jpg" />
-                </AvatarGroup>
-                <div className="mt-3 text-center">
-                    <Link to="team-detail" className="btn btn-light">
-                        Details
-                    </Link>
-                    <div className="col-md-4"></div>
+        <div className="col-3 feed-card player-details-teams m-auto border rounded shadow">
+            <div className="card m-0">
+                <h4 className="card-title text-center mt-2">Bornova Team</h4>
+                <div className="card-content card-body p-0">
+                    <AvatarGroup max={3} >
+                        <Avatar alt="Remy Sharp" src="assets/images/faces/2.jpg" />
+                        <Avatar alt="Travis Howard" src="assets/images/faces/2.jpg" />
+                        <Avatar alt="Cindy Baker" src="assets/images/faces/2.jpg" />
+                        <Avatar alt="Agnes Walker" src="assets/images/faces/2.jpg" />
+                        <Avatar alt="Trevor Henderson" src="assets/images/faces/2.jpg" />
+                    </AvatarGroup>
+                    <div className="mt-3 text-center">
+                        <Link to="team-detail" className="btn btn-light">
+                            Details
+                        </Link>
+                        <div className="col-md-4"></div>
+                </div>
             </div>
-            </div>
-            
         </div>
     </div>
     )
