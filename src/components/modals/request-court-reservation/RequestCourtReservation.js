d0f31d29143edcc73108fe82af176459a0c63abc
DESKTOP-42C5TVP\Sipay <bartu.kocakara@sipay.com.tr>
2021-06-30 09:38:55 +0300

    Request court res modal change

--- src/components/modals/request-court-reservation/RequestCourtReservation.js
+++ src/components/modals/request-court-reservation/RequestCourtReservation.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import { Modal } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
+import { BsPencilSquare } from "@react-icons/all-files/bs/BsPencilSquare";
+import { GoCreditCard } from "@react-icons/all-files/go/GoCreditCard";
+
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
 
 const RequestCourtReservation = ({handleRequestCourtReservationModalClose, show}) => {
     return (
@@ -11,53 +16,35 @@ const RequestCourtReservation = ({handleRequestCourtReservationModalClose, show}
             keyboard={false}
             >
             <Modal.Header className="court-res-header">
-                <Modal.Title>
+                <Modal.Title className="d-flex">
                 <div class="avatar me-3">
                     <img src="assets/images/faces/1.jpg" alt="" srcset="" />
                 </div>
+                <h6 className="m-auto">Court Owner</h6>
                 </Modal.Title>
             </Modal.Header>
             <Modal.Body>
-            <h6 className="text-center">Available date-times of court</h6>
-            <table class="table table-hover mb-0">
-                <tbody>
-                    <tr>
-                        <td class="text-bold-500">Michael Right</td>
-                        <td>$15/hr</td>
-                        <td class="text-bold-500">UI/UX</td>
-                        <td>Remote</td>
-                        <td>Austin,Taxes</td>
-                    </tr>
-                    <tr>
-                        <td class="text-bold-500">Morgan Vanblum</td>
-                        <td>$13/hr</td>
-                        <td class="text-bold-500">Graphic concepts</td>
-                        <td>Remote</td>
-                        <td>Shangai,China</td>
-                    </tr>
-                    <tr>
-                        <td class="text-bold-500">Tiffani Blogz</td>
-                        <td>$15/hr</td>
-                        <td class="text-bold-500">Animation</td>
-                        <td>Remote</td>
-                        <td>Austin,Texas</td>
-                    </tr>
-                    <tr>
-                        <td class="text-bold-500">Ashley Boul</td>
-                        <td>$15/hr</td>
-                        <td class="text-bold-500">Animation</td>
-                        <td>Remote</td>
-                        <td>Austin,Texas</td>
-                    </tr>
-                    <tr>
-                        <td class="text-bold-500">Mikkey Mice</td>
-                        <td>$15/hr</td>
-                        <td class="text-bold-500">Animation</td>
-                        <td>Remote</td>
-                        <td>Austin,Texas</td>
-                    </tr>
-                </tbody>
-            </table>
+                <FullCalendar
+                    plugins={[ dayGridPlugin ]}
+                    initialView="dayGridMonth"
+                    height="50vh"
+                />
+                <div clasName="form-group m-4">
+                    <div class="d-flex">
+                        <h6 className="text-center m-4">You can send note to court owner</h6><BsPencilSquare className="mt-4" size={25}/>
+                    </div>
+                    <textarea className="form-control" rows="5" placeholder="Write something"></textarea>
+                </div>
+                <div clasName="form-group m-4">
+                    <div class="d-flex">
+                    <h6 className="text-center m-4">Select Payment Type</h6><GoCreditCard className="mt-4" size={25}/>
+                    </div>
+                    <select className="form-control">
+                        <option style={{ fontWeight:"bold", color:"#25396f" }}>Sipay</option>
+                        <option style={{ fontWeight:"bold", color:"#25396f" }}>Debit Card</option>
+                        <option style={{ fontWeight:"bold", color:"#25396f" }}>Credit Card</option>
+                    </select>
+                </div>
             </Modal.Body>
             <Modal.Footer>
             <Button variant="danger" onClick={handleRequestCourtReservationModalClose}>
