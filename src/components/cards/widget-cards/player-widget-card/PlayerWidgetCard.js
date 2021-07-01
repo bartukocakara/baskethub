3cc81a0c25f02217f522535f4650813aa021e320
DESKTOP-42C5TVP\Sipay <bartu.kocakara@sipay.com.tr>
2021-07-01 16:11:34 +0300

    react-toastify installed

--- src/components/cards/widget-cards/player-widget-card/PlayerWidgetCard.js
+++ src/components/cards/widget-cards/player-widget-card/PlayerWidgetCard.js
@@ -4,14 +4,28 @@ import {
     Link
   } from "react-router-dom";
 import "./_playerWidgetCard.scss";
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 const PlayerWidgetCard = () => {
+    const notify = () => toast("Wow so easy!");
+    // 
     return (
-        <Link to="player-detail">
+        <>
+        <ToastContainer  
+            className="relative flex p-1  rounded-md justify-between overflow-hidden cursor-pointer"
+            bodyClassName={() => "text-sm font-white block p-3"}
+            position="top-right"
+            autoClose={3000}
+            />
+            <Link>
             <div className="card-body border b-primary m-2 widget-player-card d-flex p-3">
-                <FiPlusSquare className="widget-player-icon" color="#FA8602"/>
+                <button onClick={notify}>
+                <FiPlusSquare className="widget-player-icon" color="#FA8602" />
+                </button>
                 <div className="widget-player-text">
+                    <Link to="player-detail">
                     <h6 className="">Bartu Kocakara</h6>
+                    </Link>
                     <p className="text-muted">İstanbul</p>
                 </div>
                 <div className="avatar avatar-lg me-2 widget-card-right">
@@ -19,6 +33,7 @@ const PlayerWidgetCard = () => {
                 </div>
             </div>
         </Link>
+        </>
     )
 }
 
