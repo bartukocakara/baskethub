88f95fc2379d403734c58a1450947ddc5464bacb
DESKTOP-42C5TVP\Sipay <bartu.kocakara@sipay.com.tr>
2021-07-02 21:15:44 +0300

    checkout page

--- src/components/screens/checkout/Checkout.js
+++ src/components/screens/checkout/Checkout.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Checkout = () => {
     return (
         <div>
-            <h1>Court Reservation / Complete Payment </h1>
+            <h1>Court Reservation / Complete Payment Page </h1>
         </div>
     )
 }
