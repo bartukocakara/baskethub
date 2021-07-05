75b8e5e380b23a816f8278338e2570bb6335d843
DESKTOP-42C5TVP\Sipay <bartu.kocakara@sipay.com.tr>
2021-07-05 15:24:50 +0300

    Team page edited

--- src/components/card-details/my-team-detail/MyTeamDetail.js
+++ src/components/card-details/my-team-detail/MyTeamDetail.js
@@ -1,34 +1,72 @@
-import React from 'react'
-
+import React from 'react';
+import { GiPalmTree } from "@react-icons/all-files/gi/GiPalmTree";
+import { GiHealthNormal } from "@react-icons/all-files/gi/GiHealthNormal";
+import { FaCalendarCheck } from "@react-icons/all-files/fa/FaCalendarCheck";
+import { FiShare2 } from "@react-icons/all-files/fi/FiShare2";
+import { FiMail } from "@react-icons/all-files/fi/FiMail";
+import { FaHandshake } from "@react-icons/all-files/fa/FaHandshake";
+import {
+    Link
+  } from "react-router-dom";
 const MyTeamDetail = () => {
     return (
         <>
-            <div class="col-6">
+            <div class="col-10 m-auto">
                 <div class="card">
                     <div class="card-header">
-                    {
-                        [...Array(5)].map((teamDetail) => (
-                            <>
-                                <div class="media d-flex align-items-center">
-                                    <div class="avatar me-3">
-                                        <img src="assets/images/faces/1.jpg" alt="" srcset="" />
-                                        <span class="avatar-status bg-success"></span>
-                                    </div>
-                                    <div class="name flex-grow-1">
-                                        <h6 class="mb-0">Fred</h6>
-                                        <span class="text-xs">Online</span>
-                                    </div>
-                                    <button class="btn btn-sm">
-                                        <i data-feather="x"></i>
-                                    </button>
-                                </div>
-                            <hr />
-                            </>
-                        ))
-                    }
-                        
+                        <h4 className="text-center">Bornova Team Detail</h4>
                     </div>
-                    
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table class="table table-hover table-lg">
+                                <thead>
+                                    <tr>
+                                        <th className="text-center">Name</th>
+                                        <th className="text-center">City</th>
+                                        <th className="text-center">Position</th>
+                                        <th className="text-center">Status</th>
+                                        <th className="text-center">Actions</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        [...Array(5)].map((teamDetail) => (
+                                            <tr>
+                                            <td class="col-3">
+                                                <div class="d-flex align-items-center">
+                                                    <div class="avatar avatar-lg">
+                                                        <img src="assets/images/faces/5.jpg" alt="team-detail" />
+                                                    </div>
+                                                    <Link to="player-detail text-center"><p class="font-bold ms-4 mb-0">Si Cantik</p></Link>
+                                                </div>
+                                            </td>
+                                            <td class="col-auto">
+                                                <p class="mb-0 text-center">İzmir, İstanbul, Ankara</p>
+                                            </td>
+                                            <td class="col-auto  text-center">
+                                                <p class="mb-0 d-flex ">
+                                                    <span className="badge w-25 bg-success m-1">G</span>
+                                                    <span className="badge w-25 bg-success m-1">SG</span>
+                                                    <span className="badge w-25 bg-success m-1">SF</span>
+                                                </p>
+                                            </td>
+                                            <td class="col-auto">
+                                                <p class="mb-0 text-center"><GiPalmTree size={25} className="text-success m-1"/><GiHealthNormal size={25} className="text-danger m-1"/> <FaCalendarCheck size={25} className="text-primary m-1"/></p>
+                                            </td>
+                                            <td>
+                                                <div className="btn-group">
+                                                    <button className="btn btn-success"><FaHandshake/></button>
+                                                    <button className="btn btn-warning"><FiMail/></button>
+                                                    <button className="btn btn-primary"><FiShare2/></button>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                        ))
+                                    }
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
