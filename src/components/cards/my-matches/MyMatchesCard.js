09925bb518103c75fd67837bea1d17a3244182a6
bartukocakara <kocakarabartu@gmail.com>
2021-07-15 18:33:53 +0300

    matches card changed

--- src/components/cards/my-matches/MyMatchesCard.js
+++ src/components/cards/my-matches/MyMatchesCard.js
@@ -8,64 +8,78 @@ import { TiSocialFacebook } from "@react-icons/all-files/ti/TiSocialFacebook";
 import { AiOutlineWhatsApp } from "@react-icons/all-files/ai/AiOutlineWhatsApp";
 import { GoPerson } from "@react-icons/all-files/go/GoPerson";
 import { BsClockFill } from "@react-icons/all-files/bs/BsClockFill";
-import { IoIosPeople } from "@react-icons/all-files/io/IoIosPeople";
-import { FiUserPlus } from "@react-icons/all-files/fi/FiUserPlus";
-
+import Avatar from '@material-ui/core/Avatar';
+import AvatarGroup from '@material-ui/lab/AvatarGroup';
 import {
     Link
   } from "react-router-dom";
 const MyMatchesCard = () => {
     return (
         <>
-        <h6>HAZ 13, PAZAR</h6>
-        <div className="col-xl-12 col-md-12 col-sm-12 feed-card feed-my-matches-card">
-            <div className="card shadow">
-                <h4 className="card-title text-center m-2">Dr. Cavit Spor Salonu - İzmir/Bornova</h4>
-                <div className="card-content d-flex card-body card-custom">
-                    <div class="avatar avatar-lg me-2 card-avatar">
-                        <img src="assets/images/faces/2.jpg" alt="" srcset="" />
-                    </div>
-                    <div className="card-middle-text row">
-                        <div class="col-lg-4 text-center">
-                            <BsClockFill size={20} className="text-primary"/>
-                            <p>19:00 - 20:00</p>
+        <div className="col-lg-10 col-sm-12 m-auto">
+            <div className="col-md-12 d-flex p-1">
+                <h6 className="m-2">HAZ 13, PAZAR</h6>
+                <AvatarGroup max={3} className="m-2" >
+                    <Avatar alt="Remy Sharp" src="assets/images/faces/2.jpg" />
+                    <Avatar alt="Travis Howard" src="assets/images/faces/2.jpg" />
+                    <Avatar alt="Cindy Baker" src="assets/images/faces/2.jpg" />
+                    <Avatar alt="Agnes Walker" src="assets/images/faces/2.jpg" />
+                    <Avatar alt="Trevor Henderson" src="assets/images/faces/2.jpg" />
+                </AvatarGroup>
+                <span className="m-2">kişi katılıyor...</span>
+            </div>
+            <div className="col-xl-12 col-md-12 col-sm-12 feed-card feed-my-matches-card">
+                <div className="card shadow">
+                    <div className=" m-2 p-2 row">
+                        <div className="col-lg-9 col-sm-6">
+                            <h6 className="m-1">İzmir Turnuvası</h6>
                         </div>
-                        <div class="col-lg-4 text-center">
-                            <IoIosPeople size={25} className="text-primary"/>
-                            <p>8</p>
+                        <div className="col-lg-2 col-sm-6">
+                            <span className="d-inline m-1 badge bg-success">3 kişi aranıyor</span>
                         </div>
-                        <div class="col-lg-4 text-center">
-                            <FiUserPlus size={20} className="text-primary"/>
-                            <p>1-3</p>
+                    </div>
+                    <div className="card-content d-flex card-body card-custom row">
+                        <div className="col-lg-9 row col-sm-6 text-center">
+                            <div class="col-4 text-center">
+                                <BsClockFill size={20} className="text-primary"/>
+                                <p>19:00 - 20:00</p>
+                            </div>
+                            <div class="col-7">
+                                <h6 className="text-center">Teams</h6>
+                                <ul className="d-flex ul-team">
+                                    <li className="p-2">X Team</li>
+                                    <li className=" p-2">Y Team</li>
+                                </ul>
+                            </div>
                         </div>
-
+                        
+                        <div className="card-right-image col-lg-3 col-sm-9">
+                            <img className="img-fluid w-100 card-image" src="assets/images/samples/hall.jpg"
+                            alt="Card cap" />
+                        </div>
                     </div>
-                    <div className="card-left-image">
-                        <img className="img-fluid w-50 card-image" src="assets/images/samples/hall.jpg"
-                        alt="Card cap" />
-                    </div>
-                </div>
-                <div className="card-footer p-2">
-                    <div className="footer-left">
-                        <Link to="match-detail" className="btn btn-light">
-                            Match Details
-                        </Link>
-                    </div>
-                    <div className="footer-right d-flex">
-                        <Link to="match-chat" className="btn btn-light text-primary">
-                            <AiOutlineMail className="card-footer-icon"/>
-                        </Link>
-                        <div class="dropdown">
-                            <button class="btn btn-light text-primary" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                                <FiShare2 className="card-footer-icon" />
-                            </button>
-                            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                                <li><Link class="dropdown-item text-center">With Player <GoPerson /></Link></li>
-                                <li><Link class="dropdown-item text-center">Whatsapp <AiOutlineWhatsApp /></Link></li>
-                                <li><Link class="dropdown-item text-center">Facebook <TiSocialFacebook /></Link></li>
-                                <li><Link class="dropdown-item text-center">Twitter <TiSocialTwitter /></Link></li>
-                                <li><Link class="dropdown-item text-center">Instagram <TiSocialInstagram /></Link></li>
-                            </ul>
+                    <div className="card-footer p-2">
+                        <div className="footer-left">
+                            <Link to="match-detail" className="btn btn-light">
+                                Match Details
+                            </Link>
+                        </div>
+                        <div className="footer-right d-flex">
+                            <Link to="match-chat" className="btn btn-light text-primary">
+                                <AiOutlineMail className="card-footer-icon"/>
+                            </Link>
+                            <div class="dropdown">
+                                <button class="btn btn-light text-primary" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <FiShare2 className="card-footer-icon" />
+                                </button>
+                                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                                    <li><Link class="dropdown-item text-center">With Player <GoPerson /></Link></li>
+                                    <li><Link class="dropdown-item text-center">Whatsapp <AiOutlineWhatsApp /></Link></li>
+                                    <li><Link class="dropdown-item text-center">Facebook <TiSocialFacebook /></Link></li>
+                                    <li><Link class="dropdown-item text-center">Twitter <TiSocialTwitter /></Link></li>
+                                    <li><Link class="dropdown-item text-center">Instagram <TiSocialInstagram /></Link></li>
+                                </ul>
+                            </div>
                         </div>
                     </div>
                 </div>
