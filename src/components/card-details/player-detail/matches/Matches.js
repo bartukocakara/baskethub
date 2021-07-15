a10e09ce23109de478e4b6dd659fe1007e4d4f33
DESKTOP-42C5TVP\Sipay <bartu.kocakara@sipay.com.tr>
2021-07-16 02:37:09 +0300

    player match comp changed

--- src/components/card-details/player-detail/matches/Matches.js
+++ src/components/card-details/player-detail/matches/Matches.js
@@ -9,7 +9,7 @@ import { AiOutlineFieldTime } from "@react-icons/all-files/ai/AiOutlineFieldTime
 const Matches = () => {
     return (
         <>
-        <div>
+        <div className="col-md-10 col-sm-12 m-auto">
         <h6 className="text-center">14 Haziran</h6>
         <div class="card component-card_8 m-auto mb-3">
             <div class="card-body p-3">
@@ -17,36 +17,26 @@ const Matches = () => {
                     <div class="progress-order-body">
                         <div class="row mt-4">
                             <div class="col-md-12 row">
-                                <ul class="list-inline col-md-2 badge-collapsed-img mb-0 mb-3">
-                                <h6 className="text-dark"><Link to="ended-match-detail">Details</Link></h6>
-
-                                    <li class="list-inline-item">
-                                        <Link to="team-detail">Team 1 </Link>
-                                    </li>
-                                    <li class="list-inline-item">
-                                        <Link to="team-detail">Team 2</Link>
-                                    </li>
-                                </ul>
-                                <div className=" col-md-3 text-center">
-                                    <h6 className="ml-3">Alsancak Spor salonu</h6>
+                                <div class="col-md-1">
+                                    <h6><Link to="ended-match-detail" className="p-2 border border-primary bg-primary rounded text-light">i</Link></h6>
+                                </div>
+                                <div className="col-md-3 text-center">
+                                    <h6 className="ml-3"><Link to="court-detail">Alsancak Spor salonu</Link></h6>
                                 </div>
                                 <div className=" col-md-3 text-center">
                                 <AiOutlineFieldTime size={25} className="text-primary m-1"/>
                                     <p className="prop-name w-25 p-2 badge "></p>
                                     <h6 className="ml-3">15:00 - 16:00</h6>
                                 </div>
-                                <AvatarGroup max={4} className="col-md-4 text-center">
+                                <div className="col-md-5 d-flex">
+                                <AvatarGroup max={4} className="text-center m-1">
                                     <Avatar alt="Remy Sharp" src="assets/images/faces/2.jpg" />
                                     <Avatar alt="Travis Howard" src="assets/images/faces/2.jpg" />
                                     <Avatar alt="Cindy Baker" src="assets/images/faces/2.jpg" />
                                     <Avatar alt="Agnes Walker" src="assets/images/faces/2.jpg" />
                                     <Avatar alt="Trevor Henderson" src="assets/images/faces/2.jpg" />
                                 </AvatarGroup>
-                            </div>
-                            <div class="col-md-12 text-right">
-                                <span class="p-o-percentage mr-4 text-primary">60%</span>
-                                <div class="progress p-o-progress mt-2">
-                                    <div class="progress-bar bg-primary" role="progressbar" style={{ width: "60%" }} aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"></div>
+                                <p className="m-1">player joined</p>
                                 </div>
                             </div>
                         </div>
