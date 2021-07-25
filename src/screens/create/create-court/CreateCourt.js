14ace2f62f508abfc2e0e7d3c907336a63862a87
bartukocakara <kocakarabartu@gmail.com>
2021-07-26 00:41:17 +0300

    Create court form input changes

--- src/screens/create/create-court/CreateCourt.js
+++ src/screens/create/create-court/CreateCourt.js
@@ -43,6 +43,16 @@ const CreateCourt = () => {
                         <small className="text-muted"><i> 2 hourly</i></small>
                         <input type="number" className="form-control w-75 p-2" id="helpInputTop" placeholder="200,0₺" />
                     </div>
+                    <div className="form-group col-md-6 p-2">
+                        <label for="helpInputTo" className="p-2">Monthly Subscription </label>
+                        <small className="text-muted"><i>Once a week</i></small>
+                        <input type="number" className="form-control w-75 p-2" id="helpInputTop" placeholder="300,0₺" />
+                    </div>
+                    <div className="form-group col-md-6 p-2">
+                        <label for="helpInputTo" className="p-2">Yearly Subscription </label>
+                        <small className="text-muted"><i>Once a month</i></small>
+                        <input type="number" className="form-control w-75 p-2" id="helpInputTop" placeholder="1200,0₺" />
+                    </div>
                     <div className="form-group col-md-6 p-2 m-auto">
                         <label for="helpInputTo" className="p-2">Court Images </label>
                         <input type="file" className="form-control" id="helpInputTop" placeholder="200,0₺" />
